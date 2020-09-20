ca558071b2bf12af9a7aa17b401c38fc4b56c6a8
Giovanni Caracho <giovanniac>
2020-09-20 14:05:06 -0300

    layout ajustado

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,11 +4,11 @@
       <q-card-section>
 
         <div class="row q-pa-sm q-mb-md items-center justify-start">
-          <div class="col-3">
+          <div class="col-4">
             <q-select
               v-model="model.selector"
               label="Assunto"
-              :options="[{subject: '', label: 'Todos'},{ subject: 'vue', label: 'VueJS' }, { subject: 'js', label: 'Javascript' }, { subject: 'css', label: 'CSS3' }, { subject: 'html', label: 'HTML5' }]"
+              :options="[{ subject: '', label: 'Todos' }, { subject: 'vue', label: 'VueJS' }, { subject: 'js', label: 'Javascript' }, { subject: 'css', label: 'CSS3' }, { subject: 'html', label: 'HTML5' }]"
               option-value="subject"
               option-label="label"
               map-options
@@ -38,10 +38,10 @@
           </div>
         </div>
 
-        <div class="row q-gutter-md q-pa-sm">
+        <div class="row q-col-gutter-md">
           <div
             v-for="item in filteredPosts"
-            class="col-4"
+            class="col-sm-4"
             :key="item.title"
           >
             <q-card>
@@ -64,6 +64,13 @@
               <q-card-section class="q-pt-none">
                 {{ item.text }}
               </q-card-section>
+
+              <q-card-section class="row justify-end">
+                <q-btn
+                  label="Ler mais"
+                  color="primary"
+                />
+              </q-card-section>
             </q-card>
           </div>
         </div>
@@ -77,7 +84,7 @@ export default {
   name: 'PageIndex',
   data () {
     return {
-      lorem: 'abençoa nois',
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec odio dolor.',
       icons: { vue: 'fab fa-vuejs', js: 'fab fa-js-square', css: 'fab fa-css3', html: 'fab fa-html5' },
       model: { selector: '' }
     }
@@ -85,18 +92,18 @@ export default {
   computed: {
     posts () {
       return [
-        { title: 'Progamando com VUE', subject: 'vue', link: 'link', text: 'teste de texto', img: 'https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg?w=1390&crop=1' },
-        { title: 'Progamando com JS', subject: 'js', link: 'link', text: 'teste de texto', img: 'https://s30776.pcdn.co/wp-content/uploads/2020/04/AdobeStock_305233591.jpeg' },
-        { title: 'Progamando com CSS', subject: 'css', link: 'link', text: 'teste de texto', img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=980:*' },
-        { title: 'Progamando com HTML', subject: 'html', link: 'link', text: 'teste de texto', img: 'https://static1.patasdacasa.com.br/articles/5/14/55/@/6702-cachorro-beagle-a-raca-pode-apresentar-articles_media_desktop-2.jpg' },
-        { title: 'Testando VUE', subject: 'vue', link: 'link', text: 'teste de texto', img: 'https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg?w=1390&crop=1' },
-        { title: 'Testando JS', subject: 'js', link: 'link', text: 'teste de texto', img: 'https://s30776.pcdn.co/wp-content/uploads/2020/04/AdobeStock_305233591.jpeg' },
-        { title: 'Testando CSS', subject: 'css', link: 'link', text: 'teste de texto', img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=980:*' },
-        { title: 'Testando HTML', subject: 'html', link: 'link', text: 'teste de texto', img: 'https://static1.patasdacasa.com.br/articles/5/14/55/@/6702-cachorro-beagle-a-raca-pode-apresentar-articles_media_desktop-2.jpg' },
-        { title: 'Estudando o VUE', subject: 'vue', link: 'link', text: 'teste de texto', img: 'https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg?w=1390&crop=1' },
-        { title: 'Estudando o JS', subject: 'js', link: 'link', text: 'teste de texto', img: 'https://s30776.pcdn.co/wp-content/uploads/2020/04/AdobeStock_305233591.jpeg' },
-        { title: 'Estudando o CSS', subject: 'css', link: 'link', text: 'teste de texto', img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=980:*' },
-        { title: 'Estudando o HTML', subject: 'html', link: 'link', text: 'teste de texto', img: 'https://static1.patasdacasa.com.br/articles/5/14/55/@/6702-cachorro-beagle-a-raca-pode-apresentar-articles_media_desktop-2.jpg' }
+        { title: 'Progamando com VUE', subject: 'vue', link: 'link', text: this.lorem, img: 'https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg?w=1390&crop=1' },
+        { title: 'Progamando com JS', subject: 'js', link: 'link', text: this.lorem, img: 'https://s30776.pcdn.co/wp-content/uploads/2020/04/AdobeStock_305233591.jpeg' },
+        { title: 'Progamando com CSS', subject: 'css', link: 'link', text: this.lorem, img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=980:*' },
+        { title: 'Progamando com HTML', subject: 'html', link: 'link', text: this.lorem, img: 'https://static1.patasdacasa.com.br/articles/5/14/55/@/6702-cachorro-beagle-a-raca-pode-apresentar-articles_media_desktop-2.jpg' },
+        { title: 'Testando VUE', subject: 'vue', link: 'link', text: this.lorem, img: 'https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg?w=1390&crop=1' },
+        { title: 'Testando JS', subject: 'js', link: 'link', text: this.lorem, img: 'https://s30776.pcdn.co/wp-content/uploads/2020/04/AdobeStock_305233591.jpeg' },
+        { title: 'Testando CSS', subject: 'css', link: 'link', text: this.lorem, img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=980:*' },
+        { title: 'Testando HTML', subject: 'html', link: 'link', text: this.lorem, img: 'https://static1.patasdacasa.com.br/articles/5/14/55/@/6702-cachorro-beagle-a-raca-pode-apresentar-articles_media_desktop-2.jpg' },
+        { title: 'Estudando o VUE', subject: 'vue', link: 'link', text: this.lorem, img: 'https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg?w=1390&crop=1' },
+        { title: 'Estudando o JS', subject: 'js', link: 'link', text: this.lorem, img: 'https://s30776.pcdn.co/wp-content/uploads/2020/04/AdobeStock_305233591.jpeg' },
+        { title: 'Estudando o CSS', subject: 'css', link: 'link', text: this.lorem, img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=980:*' },
+        { title: 'Estudando o HTML', subject: 'html', link: 'link', text: this.lorem, img: 'https://static1.patasdacasa.com.br/articles/5/14/55/@/6702-cachorro-beagle-a-raca-pode-apresentar-articles_media_desktop-2.jpg' }
       ]
     },
     filteredPosts () {
